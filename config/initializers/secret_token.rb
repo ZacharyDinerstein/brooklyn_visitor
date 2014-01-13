4ba9d6700dc323a7732826f9dbc042615b487ca3
Zack D <zackdhome@gmail.com>
2014-01-13 15:15:40 -0800

    brooklyn visitor initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrooklynVisitor::Application.config.secret_token = '5dd328eb585e22120f6dcd0166c5f955424a7435224029d36177a7d3278274a0741002859dc1ecfe775477a2bf936907c88bdf2e1c16c370ec596e2b46eb7912'
